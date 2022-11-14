d3f0a6df72741a17608cea2ec64c3507c8ed9ed5
mesteri <79940232+mesteri@users.noreply.github.com>
2022-11-14 16:05:09 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Script Loaded');
+var signUpForm = document.querySelector('#signUpForm');
+var userNameField = document.querySelector('#userNameField');
+var userNameErrorMessage = document.querySelector('#userNameErrorMessage');
+
+signUpForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+    if(signUpForm.username.value ){
+        userNameField.classList.add('errorField');    
+        userNameErrorMessage.classList.remove('hidden');
+        console.log('You need to enter a username');
+    }
+
+    console.log(signUpForm.username.value);
+});
